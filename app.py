3ca7bac3560ab3ed2efab723cde00898d10e08c1
Jacqui De Sa <jdesa@mit.edu>
2014-09-14 05:02:41 -0400

    Fix after discussing potential errors with SmartThings

--- app.py
+++ app.py
@@ -7,10 +7,24 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+#checks the status of a smartthings device
 @app.route("/status")
 def status():
     r = requests.get("https://graph.api.smartthings.com/api/smartapps/installations/44494efe-0369-4b8d-99c3-1cb935c344fa/switch", auth=('ckskylight@gmail.com', 'Hamster111'))
     return jsonify(r.json()[0])
+
+#controls a device
+@app.route("/control", methods=['PUT'])
+def control():
+    print "Hamster1"
+
+    r = requests.put("https://graph.api.smartthings.com/api/smartapps/installations/44494efe-0369-4b8d-99c3-1cb935c344fa/switch", {"value":"on"}, auth=('ckskylight@gmail.com', 'Hamster111'))
+    print "Hamster Signal"
+    print r
+    print r.json()
+    print "Hamster2"
+    return "Hamster3"
+
 
 #return flask.jsonify(**f)
 
--- static/js/swipefunctions.js
+++ static/js/swipefunctions.js
@@ -1,10 +1,25 @@
 var container = $('.index'),
     scrollTo = $('#about');
 
-function ajaxCall() {
+function ajaxGet() {
     $.ajax({
   type: "GET",
   url: "/status",
+//        // actual url /status
+//        "https://graph.api.smartthings.com/api/smartapps/installations/44494efe-0369-4b8d-99c3-1cb935c344fa/switch",
+  dataType: 'json',
+//  data: {foo: "bar"},
+  success: function (data){
+      console.log(data);
+      alert(data["value"]);
+  }
+});
+}
+
+function ajaxPut() {
+    $.ajax({
+  type: "PUT",
+  url: "/control",
 //        // actual url /status
 //        "https://graph.api.smartthings.com/api/smartapps/installations/44494efe-0369-4b8d-99c3-1cb935c344fa/switch",
   dataType: 'json',
