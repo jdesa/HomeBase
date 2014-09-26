de8bd83cfe4bd29d7253af0a6867162a5346ec71
CK Ong <ongck@mit.edu>
2014-09-26 08:52:16 -0400

    changed oauth key

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def hello():
 #checks the status of a smartthings device
 @app.route("/status")
 def status():
-    r = requests.get("https://graph.api.smartthings.com/api/smartapps/installations/44494efe-0369-4b8d-99c3-1cb935c344fa/switch", auth=('ckskylight@gmail.com', 'Hamster111'))
+    r = requests.get("https://graph.api.smartthings.com/api/smartapps/installations/44aa6888-7a4c-4612-a9d2-e51f495a75e3/switch", auth=('ckskylight@gmail.com', 'Hamster111'))
     return jsonify(r.json()[0])
 
 #controls a device
@@ -18,7 +18,7 @@ def status():
 def control():
     print "Hamster1"
 
-    r = requests.put("https://graph.api.smartthings.com/api/smartapps/installations/44494efe-0369-4b8d-99c3-1cb935c344fa/switch", {"value":"on"}, auth=('ckskylight@gmail.com', 'Hamster111'))
+    r = requests.put("https://graph.api.smartthings.com/api/smartapps/installations/44aa6888-7a4c-4612-a9d2-e51f495a75e3/switch", {"value":"on"}, auth=('ckskylight@gmail.com', 'Hamster111'))
     print "Hamster Signal"
     print r
     print r.json()
